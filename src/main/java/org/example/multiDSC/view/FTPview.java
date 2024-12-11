0adbc451ced6c302f318db45ab7b92ff47a714c2
Campte37 <josemauelcamposl.sanjosemlg@fundacionloyola.net>
2024-12-11 12:13:31 +0100

    Commit ventana FTP

---
+++ src/main/java/org/example/multiDSC/view/FTPview.java
@@ -0,0 +1,26 @@
+package org.example.multiDSC.view;
+
+import javax.swing.*;
+/*FTPview - Main view for the FTP functions of the app.*
+@author Jose Manuel Campos Lopez
+@version 1.0*/
+public class FTPview {
+    public FTPview() {
+    crearVentanaFTP();
+    }
+    private JScrollPane panelTree;
+    private JTree tree;
+    private JButton gestionarPermisosButton;
+    private JButton subirArchivoButton;
+    private JButton descargarArchivoButton;
+    private JButton gestionarDirectoriosButton;
+    private JButton salirButton;
+    private JPanel panelMain;
+
+
+
+    private void crearVentanaFTP(){
+        panelMain.setSize(500,500);
+
+    }
+}
